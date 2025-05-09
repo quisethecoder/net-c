dc9992bd5ddb9c96e4ee9271d79d3194137adc92
Marquise D <marquise_dawkins@aol.com>
2025-05-08 23:13:41 -0400

    arrays

---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //arrays not structs
+            var numbers = new int[3];
+            numbers[0] = 1;
+            numbers[1] = 2;
+            numbers[2] = 3;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+            
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            var names = new string[3] { "Jack", "John", "Mary" };
+        }
+    }
+}
