1e427018e5ba5e02aa8b769ec217f06c8946cbe0
Marquise D <marquise_dawkins@aol.com>
2025-05-08 22:59:47 -0400

    learning operators

---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 2;
+            var c = 3;
+
+            Console.WriteLine(c > b && a < c);
+        }
+    }
+}
