4fe2eadfda5aca27af763ed919850025d6a39556
Marquise D <marquise_dawkins@aol.com>
2025-05-08 22:39:38 -0400

    TypeConversions

---
+++ TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //  byte b = 1;
+            //int i = b;
+            //Console.WriteLine(i);
+
+            //int i = 1;
+            //byte b = (byte) i;
+            //Console.WriteLine(b);
+
+            try
+            {
+                var number = "1234";
+                byte b = Convert.ToByte(number);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Number could not be converted to a byte");
+                
+            }
+
+           
+            //int i = Convert.ToInt32(number);
+            
+
+        }
+    }
+}
---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const float Pi = 3.14f;
+            var number = 1;
+            var firstName = "John";
+            var letter = 'a';
+            var price = 20.95f;
+
+            Console.WriteLine(number);
+            Console.WriteLine(firstName);
+            Console.WriteLine(letter);
+            Console.WriteLine(price);
+
+        }
+    }
+}
