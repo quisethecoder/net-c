73279ba39a4d011a0f32bbbf96625a857d92fbb9
Marquise D <marquise_dawkins@aol.com>
2025-05-09 20:03:11 -0400

    Random

---
+++ Random/Random/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Random;
+
+namespace Random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+             var random = new System.Random();
+             const int passwordLength = 10;
+             var buffer = new char[passwordLength];
+             for (var i = 0; i < passwordLength; i++)
+             {
+                 buffer[i] = (char)('a' + random.Next(0, 26));
+             }
+             var password = new string(buffer);
+             Console.WriteLine(password);
+
+       
+            
+        }
+    }
+}
+
