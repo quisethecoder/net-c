f15070f1d3cdabb491d994d39523d8bd0d9648ad
Marquise D <marquise_dawkins@aol.com>
2025-05-09 23:29:58 -0400

    Strings

---
+++ WorkingWithText/WorkingWithText/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Globalization;
+
+namespace WorkingWithText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "Marquise Dawkins ";
+            Console.WriteLine("Trim: '{0}'", fullName.Trim());
+            Console.WriteLine("ToUpper: '{0}'", fullName.Trim().ToUpper());
+
+            var index = fullName.IndexOf(' ');
+            var firstName = fullName.Substring(0, index);
+            var lastName = fullName.Substring(index + 1);
+            Console.WriteLine("FirstName: " + firstName);
+            Console.WriteLine("LastName: " + lastName);
+
+            var names = fullName.Split(' ');
+            Console.WriteLine("FirstName: " + names[0]);
+            Console.WriteLine("LastName: " + names[1]);
+
+            
+            Console.WriteLine(fullName.Replace("Marquise", "Kayon"));
+            if (String.IsNullOrWhiteSpace(" "))
+            {
+                Console.WriteLine("Invalid");
+            }
+            else
+            {
+                Console.WriteLine("Valid");
+            }
+
+            var str = "25";
+            Console.WriteLine(Convert.ToInt32(str));
+            var age = Convert.ToByte(str);
+            Console.WriteLine(age);
+
+            float price = 29.95f;
+            Console.WriteLine(price.ToString("C0"));
+        }
+    }
+}
+
