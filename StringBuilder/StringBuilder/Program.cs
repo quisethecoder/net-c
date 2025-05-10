22f778ddbb395575fb19d19cad45d7a5af768cfd
Marquise D <marquise_dawkins@aol.com>
2025-05-10 00:11:11 -0400

    StringBuilder

---
+++ StringBuilder/StringBuilder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace StringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new System.Text.StringBuilder("Hello World");
+            builder.Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0,20)
+                .Insert(0, new string('-', 10));
+            
+            Console.WriteLine(builder);
+
+            Console.WriteLine("FirstChar: " + builder[0]);
+        }
+    }
+}
+
